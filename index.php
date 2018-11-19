7f552313804e040bd1f9066bea617a738d4b7365
nicolaskesh22 <nicolaskesh22@hotmail.com>
2018-11-18 21:34:10 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>aqui Cochamo</title>
+		<link type="text/css" rel="stylesheet" href="css/materialize.min.css" rel="stylesheet">
+	</head>
+	<nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">Logo</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">JavaScript</a></li>
+      </ul>
+    </div>
+  </nav>
+	<body>
+
+
+
+	</body>
+	<script type="text/javascript" src="js/materialize.min.js"></script>
+</html>
